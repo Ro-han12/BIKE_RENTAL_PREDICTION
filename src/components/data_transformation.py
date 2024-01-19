2fce3ef7712796f9380726cadb7b51db75cc80cc
rohan <rohannair2939@gmail.com>
2024-01-19 18:45:07 +0530

    model_trainer done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ from src.exception import CustomException
 from src.logger import logging
 import os
 
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class DataTransformationConfig:
@@ -49,51 +49,104 @@ class DataTransformation:
 
         except Exception as e:
             raise CustomException(e, sys)
-
-    def initiate_data_transformation(self, train_path, test_path):
+        
+         
+    def initiate_data_transformation(self,train_path,test_path):
 
         try:
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
 
             logging.info("Obtaining preprocessing object")
 
-            preprocessing_obj = self.get_data_transformer_object()
+            preprocessing_obj=self.get_data_transformer_object()
 
-            x = train_df.drop(columns=['cnt','dteday'],axis=1)
-            y = train_df['cnt']
+            target_column_name='cnt'
+            numerical_columns = ['instant', 'season', 'yr', 'mnth', 'holiday', 'weekday',
+                                  'workingday', 'weathersit', 'temp', 'atemp', 'hum', 'windspeed',
+                                  'casual', 'registered']
 
-            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+            input_feature_train_df=train_df.drop(columns=[target_column_name,'dteday'],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            x_train_transformed = preprocessing_obj.fit_transform(x_train)
-            x_test_transformed = preprocessing_obj.transform(x_test)
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            logging.info(f"Applying StandardScaler on transformed data.")
-
-            sc = StandardScaler()
-            x_train_transformed = sc.fit_transform(x_train_transformed)
-            x_test_transformed = sc.transform(x_test_transformed)
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
 
             save_object(
+
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
+
             )
 
             return (
-                x_train_transformed,
-                x_test_transformed,
-                y_train,
-                y_test,
+                train_arr,
+                test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
-
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
+
+    # def initiate_data_transformation(self, train_path, test_path):
+
+    #     try:
+    #         train_df = pd.read_csv(train_path)
+    #         test_df = pd.read_csv(test_path)
+
+    #         logging.info("Read train and test data completed")
+
+    #         logging.info("Obtaining preprocessing object")
+
+    #         preprocessing_obj = self.get_data_transformer_object()
+
+    #         x = train_df.drop(columns=['cnt','dteday'],axis=1)
+    #         y = train_df['cnt']
+
+    #         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    #         logging.info(
+    #             f"Applying preprocessing object on training dataframe and testing dataframe."
+    #         )
+
+    #         x_train_transformed = preprocessing_obj.fit_transform(x_train)
+    #         x_test_transformed = preprocessing_obj.transform(x_test)
+
+    #         logging.info(f"Applying StandardScaler on transformed data.")
+
+    #         sc = StandardScaler()
+    #         x_train_transformed = sc.fit_transform(x_train_transformed)
+    #         x_test_transformed = sc.transform(x_test_transformed)
+
+    #         logging.info(f"Saved preprocessing object.")
+
+    #         save_object(
+    #             file_path=self.data_transformation_config.preprocessor_obj_file_path,
+    #             obj=preprocessing_obj
+    #         )
+
+    #         return (
+    #             x_train_transformed,
+    #             x_test_transformed,
+    #             y_train,
+    #             y_test,
+    #             self.data_transformation_config.preprocessor_obj_file_path,
+    #         )
+
+    #     except Exception as e:
+    #         raise CustomException(e, sys)
